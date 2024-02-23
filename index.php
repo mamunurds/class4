643355085c06eb115a410487151fa7e671e8283d
mamun <mamunurds@gmail.com>
2024-02-23 22:58:37 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,317 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg fixed-top navbar-dark navbar-custom">
+        <div class="container">
+            <a class="navbar-brand" href="#home">Md. Mamunur Rashid</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="#home">Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#services">Gallary</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+        <!-- end Navigation -->
+
+        <!-- Masthead -->
+        <header class="masthead text-white text-center" id="home">
+        <div class="overlay">
+            <!-- Add a tint to the background image -->
+        </div>
+
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-9 mx-auto">
+                <h1 class="heading-text mb-3">
+                Hi! I'm Mamun, a Web-designer. Welcome to my portfolio!
+                </h1>
+                <a href="#portfolio" class="btn btn-lg btn-primary px-4 text-uppercase">
+                    My Portfolio
+                </a>
+            </div>
+            </div>
+        </div>
+        </header>
+        <!-- end Masthead -->
+
+        <!-- About Me -->
+        <section class="about" id="about">
+        <div class="container">
+            <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <div class="p-5">
+                <img src="image/mamun.png" alt="" class="img-fluid rounded-circle">
+                </div>
+            </div>
+
+            <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                <h1 class="heading-text">
+                    About Me
+                </h1>
+                <p class="lead">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <!-- end About Me -->
+
+        <!-- Services Section -->
+        <section class="services text-center text-white" id="services">
+        <div class="overlay">
+            <!-- Add a tint to the background image -->
+        </div>
+
+        <div class="container">
+            <div class="row">
+            <div class="col-md-12">
+                <h1 class="heading-text mb-5">Services</h1>
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-lg-4">
+                <div class="service-icon d-flex">
+                <i class="icon-diamond text-primary m-auto"></i>
+                </div>
+
+                <h3>Beautiful Design</h3>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam.</p>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="service-icon d-flex">
+                <i class="icon-screen-desktop text-primary m-auto"></i>
+                </div>
+
+                <h3>Fully Responsive</h3>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam.</p>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="service-icon d-flex">
+                <i class="icon-support text-primary m-auto"></i>
+                </div>
+
+                <h3>Full Support</h3>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam.</p>
+            </div>
+            </div>
+        </div>
+        </section>
+        <!-- end Services Section -->
+
+        <!-- Portfolio Section -->
+        <section class="portfolio bg-light" id="portfolio">
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-9 mx-auto">
+                <div class="portfolio-header text-center">
+                <h1 class="heading-text">My Portfolio</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="container-fluid p-0 bg-white">
+            <!-- Portfolio Item 1 -->
+            <div class="row no-gutters">
+            <div class="col-lg-6 order-lg-2 portfolio-image item-1">
+                <!-- Portfolio Image in here -->
+            </div>
+
+            <div class="col-lg-6 order-lg-1 portfolio-text my-auto">
+                <h3>Portfolio Item 1</h3>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam. Nullam vel nibh nec lacus efficitur efficitur et sit amet lacus. Vestibulum in congue ligula, sit amet condimentum elit. Donec turpis dui,
+                tincidunt non quam sit amet, aliquet posuere leo. Nunc id volutpat ligula. Donec nec felis dignissim, feugiat risus id, volutpat ex. Proin vitae luctus elit. Nam placerat euismod risus, porta sagittis neque varius quis. </p>
+            </div>
+            </div>
+
+            <!-- Portfolio Item 2 -->
+            <div class="row no-gutters">
+            <div class="col-lg-6 portfolio-image item-2">
+                <!-- Portfolio Image in here -->
+            </div>
+
+            <div class="col-lg-6 portfolio-text my-auto">
+                <h3>Portfolio Item 2</h3>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam. Nullam vel nibh nec lacus efficitur efficitur et sit amet lacus. Vestibulum in congue ligula, sit amet condimentum elit. Donec turpis dui,
+                tincidunt non quam sit amet, aliquet posuere leo. Nfunc id volutpat ligula. Donec nec felis dignissim, feugiat risus id, volutpat ex. Proin vitae luctus elit. Nam placerat euismod risus, porta sagittis neque varius quis. </p>
+            </div>
+            </div>
+
+            <!-- Portfolio Item 3 -->
+            <div class="row no-gutters">
+            <div class="col-lg-6 order-lg-2 portfolio-image item-3">
+                <!-- Portfolio Image in here -->
+            </div>
+
+            <div class="col-lg-6 order-lg-1 portfolio-text my-auto">
+                <h3>Portfolio Item 3</h3>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam. Nullam vel nibh nec lacus efficitur efficitur et sit amet lacus. Vestibulum in congue ligula, sit amet condimentum elit. Donec turpis dui,
+                tincidunt non quam sit amet, aliquet posuere leo. Nunc id volutpat ligula. Donec nec felis dignissim, feugiat risus id, volutpat ex. Proin vitae luctus elit. Nam placerat euismod risus, porta sagittis neque varius quis. </p>
+            </div>
+            </div>
+        </div>
+        </section>
+        <!-- end Portfolio Section -->
+
+        <!-- Contact Section -->
+        <section class="contact text-center bg-light" id="contact">
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-9 mx-auto">
+                <h1>Get in contact!</h1>
+                <p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula libero vitae vulputate aliquam. Nullam vel nibh nec lacus efficitur efficitur et sit amet lacus. Vestibulum in congue ligula, sit amet condimentum elit.</p>
+            </div>
+            </div>
+
+            <div class="row mt-5">
+            <div class="col-md-4 ml-auto">
+                <div class="contact-icon d-flex">
+                <i class="icon-phone text-primary m-auto"></i>
+                </div>
+                <h3>07123 456789</h3>
+            </div>
+
+            <div class="col-md-4 mr-auto">
+                <div class="contact-icon d-flex">
+                <i class="icon-envelope-open text-primary m-auto"></i>
+                </div>
+                <h3>email@domain.com</h3>
+            </div>
+            </div>
+        </div>
+        </section>
+        <!-- end Contact Section -->
+
+        <!-- Footer -->
+        <footer>
+        <!--subscription box : to show subscription input-->
+        <div class="subscription-box">
+            <h3>Subscribe To Our Newletter</h3>
+            <input type="text" placeholder="send your email address">
+            <button type="submit">Submit</button>
+        </div>
+
+        <!--links provided in footer-->
+        <div class="bottom-side-links">
+            <div class="bottom-links-div">
+               <h4>Our Services</h4>
+               <hr class="nav-hr">
+                <ul>
+                    <li><span><i class="fas fa-caret-right"></i></span><a>Delivery Information</a></li>
+                    <li><span><i class="fas fa-caret-right"></i></span><a>Returns</a></li>
+                    <li><span><i class="fas fa-caret-right"></i></span><a>Terms and Conditions</a></li>
+                    <li><span><i class="fas fa-caret-right"></i></span><a>Shopping and Refunds</a></li>
+                    <li><span><i class="fas fa-caret-right"></i></span><a>Hotels and Awards</a></li>
+                    <li><span><i class="fas fa-caret-right"></i></span><a>Press Room</a></li>
+                </ul>
+            </div>
+            <div class="bottom-links-div">
+                <h4>extras</h4>
+                <hr class="nav-hr">
+                 <ul>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>contact us</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>returns</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>specails</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>discount week</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>events</a></li>
+                 </ul>
+             </div>
+             <div class="bottom-links-div">
+                <h4>my account</h4>
+                <hr class="nav-hr">
+                 <ul>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>my designs</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>my selections</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>my purchases</a></li>
+                     <li><span><i class="fas fa-caret-right"></i></span><a>my specialities</a></li>                     
+                 </ul>
+             </div> 
+                        
+        </div>
+        <!--to show app images as links and social media links-->
+        <div class="social-links-div">
+            
+            <div class="sm-box">
+                <h4>follow us on</h4>
+               <div class="sm-div">
+                   <span class="sm-span"><i class="fab fa-twitter"></i></span>
+                   <span class="sm-span"><i class="fab fa-facebook"></i></span>
+                   <span class="sm-span"><i class="fab fa-google"></i></span>
+                   <span class="sm-span"><i class="fab fa-instagram"></i></span>
+                   <span class="sm-span"><i class="fab fa-youtube"></i></span>
+               </div>
+            </div>
+        </div>
+
+        <!--bottom footer-->
+        <div class="bottom-footer">
+            <p>Developed by Md. Mamunur Rashid</p>
+        </div>
+
+    </footer>
+   
+
+
+
+
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
